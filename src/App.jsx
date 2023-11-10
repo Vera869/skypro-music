fe8b00d600768f63609819ecdaede3c8f49431e2
Vera869 <verea8910@gmail.com>
2023-11-10 20:47:23 +0700

    задание выполнено

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      Здесь скоро будет Skypro.Music
+      </header>
+    </div>
+  )
+}
+
+export default App
