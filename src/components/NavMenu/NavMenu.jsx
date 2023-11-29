f2eaab19bdd6f4c47b0212168a594441e4bd59b0
Vera869 <verea8910@gmail.com>
2023-11-29 14:01:40 +0700

    + Burger

--- src/components/NavMenu/NavMenu.jsx
+++ src/components/NavMenu/NavMenu.jsx
@@ -1,17 +1,21 @@
 import "./NavMenu.css"
+import {useState} from "react"
 
 function NavMenu () {
+  const [visible, setVisible] = useState(false);
+  const toggleVisibility = () => setVisible(!visible);
+
    return (
    <nav className="main__nav nav">
       <div className="nav__logo logo">
       <img className="logo__image" src="img/logo.png" alt="logo" />
       </div>
-      <div className="nav__burger burger">
+      <div className="nav__burger burger" onClick={toggleVisibility}>
       <span className="burger__line"></span>
       <span className="burger__line"></span>
       <span className="burger__line"></span>
       </div>
-<     div className="nav__menu menu">
+      {visible && <     div className="nav__menu menu">
         <ul className="menu__list">
           <li className="menu__item">
             <a href="#" className="menu__link">Главное</a>
@@ -23,7 +27,8 @@ function NavMenu () {
             <a href="../signin.html" className="menu__link">Войти</a>
           </li>
         </ul>
-      </div>
+      </div>}
+
     </nav>
    )
 }
