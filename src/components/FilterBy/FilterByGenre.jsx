83b1a17c73916eb8f4cc64916e1194f6cb700212
Vera869 <verea8910@gmail.com>
2023-12-05 13:10:21 +0700

    +

--- src/components/FilterBy/FilterByGenre.jsx
+++ src/components/FilterBy/FilterByGenre.jsx
@@ -1,29 +1,29 @@
-import "./FilterBy.css"
-import { useState } from "react";
+// import "./FilterBy.css"
+// import { useState } from "react";
 
-function FilterByGenre () {
+// function FilterByGenre () {
 
-  let genreList = () => {
-    return (
-      <div className="list">
-      <ul className="genre">
-        <li>Rock</li>
-        <li>Jazz</li>
-        <li>Disco</li>
-        <li>Indi</li>
-        <li>Classic</li>
-      </ul>
-    </div>
-    )
-  }
+//   let genreList = () => {
+//     return (
+//       <div className="list">
+//       <ul className="genre">
+//         <li>Rock</li>
+//         <li>Jazz</li>
+//         <li>Disco</li>
+//         <li>Indi</li>
+//         <li>Classic</li>
+//       </ul>
+//     </div>
+//     )
+//   }
  
-  const [open, setOpen] = useState(false);
+//   const [open, setOpen] = useState(false);
 
-    function toggleOpen () {
-      console.log("Genre");
-      setOpen(!open);
-    };
-   return (<><div className="filter__button button-genre _btn-text" onClick={toggleOpen}> жанру</div>
-          {open && genreList()}</>)
- }
- export default FilterByGenre
+//     function toggleOpen () {
+//       console.log("Genre");
+//       setOpen(!open);
+//     };
+//    return (<><div className="filter__button button-genre _btn-text" onClick={toggleOpen}> жанру</div>
+//           {open && genreList()}</>)
+//  }
+//  export default FilterByGenre
--- src/components/FilterBy/FilterByYear.jsx
+++ src/components/FilterBy/FilterByYear.jsx
@@ -1,34 +1,34 @@
-import "./FilterBy.css"
-import { useState } from "react";
+// import "./FilterBy.css"
+// import { useState } from "react";
 
 
-function FilterByYear () {
+// function FilterByYear () {
 
-  let yearList = () => {
-    return (
-      <div className="list">
-      <ul className="years">
-        <li>2001</li>
-        <li>1969</li>
-        <li>1987</li>
-        <li>2014</li>
-        <li>2022</li>
-      </ul>
-    </div>
-    )
-  }
+//   let yearList = () => {
+//     return (
+//       <div className="list">
+//       <ul className="years">
+//         <li>2001</li>
+//         <li>1969</li>
+//         <li>1987</li>
+//         <li>2014</li>
+//         <li>2022</li>
+//       </ul>
+//     </div>
+//     )
+//   }
  
-  const [open, setOpen] = useState(false);
+//   const [open, setOpen] = useState(false);
 
-    function toggleOpen () {
-      console.log("Year");
-      setOpen(!open);
-    };
+//     function toggleOpen () {
+//       console.log("Year");
+//       setOpen(!open);
+//     };
  
-  return (<><div className="filter__button button-year _btn-text" onClick={toggleOpen}>
-          году выпуска 
-        </div>
-        {open && yearList()}
-        </>)
-}
-export default FilterByYear
+//   return (<><div className="filter__button button-year _btn-text" onClick={toggleOpen}>
+//           году выпуска 
+//         </div>
+//         {open && yearList()}
+//         </>)
+// }
+// export default FilterByYear
