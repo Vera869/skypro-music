eb015c18b5263ab7d3654f69a7e42d2e4cd9fabc
Vera869 <verea8910@gmail.com>
2024-01-31 15:42:36 +0700

    +

--- src/pages/Favorites/favorites.jsx
+++ src/pages/Favorites/favorites.jsx
@@ -1,7 +1,38 @@
+import * as S from '../../components/TrackList/StyledTrackList.js'
+
 export const Favorites = () => {
-  return (
-    <div className="container new-page">
-      <h1>Мой плейлист</h1>
-    </div>
+  return ( <>
+  <S.MainCenterBlock>
+    <S.CenterBlockSearch>
+     <S.SearchSvg>
+       <use xlinkHref="img/icon/sprite.svg#icon-search"></use>
+     </S.SearchSvg>
+     <S.SearchText type="search" placeholder="Поиск" name="search" />
+   </S.CenterBlockSearch>
+   {/* <img src={'/' + arrayCategory.src} alt={arrayCategory.alt} /> */}
+   <S.CenterBlockH2>Мой плейлист</S.CenterBlockH2>
+   <S.ContentTitle>
+    <S.PlaylistTitleC0l01>Трек</S.PlaylistTitleC0l01>
+    <S.PlaylistTitleC0l02>ИСПОЛНИТЕЛЬ</S.PlaylistTitleC0l02>
+    <S.PlaylistTitleC0l03>АЛЬБОМ</S.PlaylistTitleC0l03>
+    <S.PlaylistTitleC0l04>
+      <S.PlaylistTitleSvg alt="time">
+        <use xlinkHref="img/icon/sprite.svg#icon-watch"></use>
+      </S.PlaylistTitleSvg>
+    </S.PlaylistTitleC0l04>
+  </S.ContentTitle>
+  </S.MainCenterBlock>
+ </>
+    
+    
   )
 }
+
+
+// export const Favorites = () => {
+//   return (
+//     <div className="container new-page">
+//       <h1>Мой плейлист</h1>
+//     </div>
+//   )
+// }
