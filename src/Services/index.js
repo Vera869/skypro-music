ae055b5b31bc8c3705a6d791c0301fe72adc77e6
Vera869 <verea8910@gmail.com>
2024-01-31 18:29:29 +0700

    +

---
+++ src/Services/index.js
@@ -0,0 +1,35 @@
+import {createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const musicApi = createApi({
+   reducerPath: "musicApi",
+   baseQuery: fetchBaseQuery({
+      baseUrl: 'https://skypro-music-api.skyeng.tech/catalog/',
+   }),
+   endpoints: (builder) => ({
+      getAllTracks: builder.query ({
+         query: () => ({url: `track/all/`})
+      }),
+      getFavTracks: builder.query ({
+         query: () => ({
+            url: `track/favorite/all/`,
+            headers: {Authorisation: ''},
+      })
+      }),
+      addFavTrack: builder.mutation ({
+         query: (id) => ({
+            url: `track/${id}/favorite/`,
+            headers: {Authorisation: ''},
+            method: 'POST'
+         })
+      }),
+      deleteFavTrack: builder.mutation ({
+         query: (id) => ({
+            url: `track/${id}/favorite/`,
+            headers: {Authorisation: ''},
+            method: 'DELETE'
+         })
+      }),
+   })
+})
+
+export const { useGetAllTracksQuery, useGetFavTracksQuery, useAddFavTrackQuery, useDeleteFavTrackQuery } = musicApi
--- src/Store/store.js
+++ src/Store/store.js
@@ -1,10 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { trackReducer } from './Slices/sliceTrack'
-// import thunk from 'redux-thunk'
+// import musicApiReducer, { musicApi } from '../Services'
+// import { buildGetDefaultMiddleware } from '@reduxjs/toolkit/dist/getDefaultMiddleware'
 
 export const store = configureStore({
   reducer: {
     tracks: trackReducer,
   },
-//   middleware: [thunk],
+//   middleware: ,
 })
+
+// export const store = configureStore({
+//   reducer: {
+//     player: playerReducer,
+//     [musicApi.reducerPath]: musicApiReducer
+//   },
+//   middleware: (GetDefaultMiddleware) => GetDefaultMiddleware().concat(musicApi.middleware),
+// })
